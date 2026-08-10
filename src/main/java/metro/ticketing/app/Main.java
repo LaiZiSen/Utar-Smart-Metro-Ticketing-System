8e71140e1e5cee5561561d72bddf24b73c9fabb1
LaiZiSen <samlai060504@gmail.com>
2026-08-10 14:50:49 +0800

    added file handling for Train Service

--- src/main/java/metro/ticketing/app/Main.java
+++ src/main/java/metro/ticketing/app/Main.java
@@ -6,33 +6,13 @@ import metro.ticketing.services.RouteService;
 import metro.ticketing.services.StationService;
 import metro.ticketing.services.TrainService;
 import metro.ticketing.services.UserService;
+import metro.ticketing.services.TicketService;
 
 public class Main {
     public static void main(String[] args) {
-        UserService uService = new UserService();
-
-        uService.viewAllUsers();
-        
-        Passenger newUser = new Passenger("blaaa", "registed", "register@gmail.com", "pass", UserRole.PASSENGER);
-
-        uService.registerUser(newUser);
-
-        uService.saveData();
-
-        // train test
-        TrainService trainService = new TrainService();
-
-        trainService.viewAllTrains();
-
-        // station test
-        StationService stationService = new StationService();
-
-        stationService.viewAllStations();
-
-        // route test
-        RouteService routeService = new RouteService(stationService);
-
-        routeService.viewAllRoutes();
-
+        StationService stService = new StationService();
+        TicketService tService = new TicketService(stService);
+    
+        tService.viewAllTicket();     
     }
 }
--- src/main/java/metro/ticketing/services/TicketService.java
+++ src/main/java/metro/ticketing/services/TicketService.java
@@ -1,5 +1,55 @@
 package metro.ticketing.services;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import metro.ticketing.repository.FileManager;
+import metro.ticketing.repository.JSONFileManager;
+import metro.ticketing.model.Ticket;
+import metro.ticketing.services.StationService;
+import metro.ticketing.services.UserService;
+
 public class TicketService {
+    private ArrayList<Ticket> tickets = new ArrayList<Ticket>();
+    private FileManager fileManager = new JSONFileManager("data/ticket.json");
+
+    public void viewAllTicket() {
+        for (Ticket ticketData: this.tickets) {
+            ticketData.printTicket();
+            System.out.println();
+        }
+    }
+
+    public TicketService(StationService stService, UserService uService) {
+        JSONArray ticketJsonArray = new JSONArray();
+
+        try {
+            ticketJsonArray = fileManager.loadData();
+        } catch (Exception e) {
+            System.out.println("Error occured when loading ticket data from json");
+        };
+
+        for (int i = 0; i < ticketJsonArray.length(); i++) {
+            JSONObject tempJsonObj = ticketJsonArray.getJSONObject(i);
+
+            this.tickets.add(Ticket.jsonToTicket(tempJsonObj, stService, uService));
+        }
+    }
+
+    public void saveData(StationService stService) {
+        JSONArray inputJsonArray = new JSONArray();
+
+        for (Ticket ticket: this.tickets) {
+            inputJsonArray.put(Ticket.ticketToJSONObject(ticket));
+        }
+        
+        try {
+            fileManager.saveData(inputJsonArray);
+        } catch (Exception e) {
+            System.out.println("Failed to save ticket data");
+        }
+    }
 
 }
