530264486d5c759d299c67f23b0d6ffaf2b61b96
eexuanliew2006-blip <eexuan.liew2006@gmail.com>
2026-08-15 15:15:25 +0800

    Implement fixed fare rates

--- src/main/java/metro/ticketing/fare/FareCalculator.java
+++ src/main/java/metro/ticketing/fare/FareCalculator.java
@@ -1,5 +1,9 @@
 package metro.ticketing.fare;
 
+import metro.ticketing.enums.TicketType;
+
 public interface FareCalculator {
+
+    double calculateFare(double distance, TicketType ticketType);
 
 }
--- src/main/java/metro/ticketing/fare/StandardFareCalculator.java
+++ src/main/java/metro/ticketing/fare/StandardFareCalculator.java
@@ -1,5 +1,36 @@
 package metro.ticketing.fare;
 
-public class StandardFareCalculator {
+import metro.ticketing.enums.TicketType;
+
+public class StandardFareCalculator implements FareCalculator {
+
+    private static final double SINGLE_RATE  = 0.60; 
+    private static final double DAILY_RATE = 1.10; 
+    private static final double MONTHLY_RATE = 11.00; 
+
+    @Override
+    public double calculateFare(double distance, TicketType ticketType){
+
+        double rate; 
+
+        switch (ticketType) {
+            case SINGLE:
+                rate = SINGLE_RATE; 
+                break;
+
+            case DAILY: 
+                rate = DAILY_RATE; 
+                break; 
+
+            case MONTHLY: 
+                rate = MONTHLY_RATE; 
+                break; 
+        
+            default:
+                    throw new IllegalArgumentException("Invalid ticket type. "); 
+        }
+
+        return distance * rate; 
+    }
 
 }
