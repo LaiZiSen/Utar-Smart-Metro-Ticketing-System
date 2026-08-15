1dbcbf3f2f35de733d24322d01b3f33923e246b5
LaiZiSen <samlai060504@gmail.com>
2026-08-15 11:50:07 +0800

    Setted up payment and payment interfacea

--- src/main/java/metro/ticketing/services/PaymentService.java
+++ src/main/java/metro/ticketing/services/PaymentService.java
@@ -1,5 +1,18 @@
 package metro.ticketing.services;
 
+import java.util.Arraylist;
+
+import metro.ticketing.repository.FileManager;
+import metro.ticketing.repository.JsonFileManager;
+import metro.ticketing.model.User;
+
 public class PaymentService {
-    
+    public PaymentService() {
+
+    }
+
+    public boolean processPayment(){
+
+    }
+
 }
