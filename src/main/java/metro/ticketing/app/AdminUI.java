4ace93face9dc6dd8f622e4bc72cba596356d520
pp8759-ui <pp8759@1utar.my>
2026-08-23 23:06:44 +0800

    restructure of train function

--- src/main/java/metro/ticketing/app/AdminUI.java
+++ src/main/java/metro/ticketing/app/AdminUI.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import metro.ticketing.include.func;
 import metro.ticketing.model.Admin;
-import metro.ticketing.model.Train;
 import metro.ticketing.services.ReportService;
 import metro.ticketing.services.RouteService;
 import metro.ticketing.services.StationService;
@@ -35,7 +34,7 @@ public class AdminUI{
                     break;
                 
                 case '2':
-                    trainMenu(trainService);
+                    trainService.run();
                     break;
                 
                 case '3':
@@ -83,65 +82,5 @@ public class AdminUI{
         System.out.println("5. Report");
         System.out.println("6. Profile");
         System.out.println("7. Logout");
-    }
-
-    private static void trainMenu(TrainService trainService){
-        while(true){
-            func.printHeader("Train System", '-');
-            System.out.println("1. View Trains");
-            System.out.println("2. Add Train");
-            System.out.println("3. Back");
-
-            char choice = func.getChoice();
-            System.out.println("");
-
-            switch(choice){
-                case '1':
-                    trainService.viewTrains();
-                    break;
-                
-                case '2':
-                    addTrain(trainService);
-                    break;
-
-                case '3':
-                    return;
-                    
-                default:
-                    System.out.println("!!!INVALID INPUT!!!\n");    
-            }
-        }
-    }
-
-    private static void addTrain(TrainService trainService){
-        String name = func.getStrLnInput("Enter train name: ");
-
-        if(trainService.isDuplicate(name)){
-            System.out.println("A train with this name already exists.\n");
-            return;
-        }
-
-        int capacity = func.getIntInput("Enter train capcity: ");
-
-        while(true){
-            System.out.println("1. Confirm");
-            System.out.println("2. Cancel");
-            char confirm = func.getChoice();
-
-            if(confirm == '1'){
-                break;
-            }else if(confirm == '2'){
-                System.out.println("Add train cancelled.\n");
-                return;
-            }else{
-                System.out.println("!!!INVALID INPUT!!!\n");
-            }
-        }
-
-        Train newTrain = new Train(trainService.nextId(), name, capacity);
-        trainService.addTrain(newTrain);
-
-        trainService.saveData();
-        System.out.println("Train added successfully\n");
     }
 }
--- src/main/java/metro/ticketing/services/TrainService.java
+++ src/main/java/metro/ticketing/services/TrainService.java
@@ -76,4 +76,83 @@ public class TrainService {
     public void addTrain(Train train){
         this.trains.add(train);
     }
+
+    public void run(){
+        while(true){
+            trainMenu();
+
+            char choice = func.getChoice();
+            System.out.println("");
+
+            switch(choice){
+                case '1':
+                    viewTrain();
+                    break;
+
+                case '2':
+                    addTrain();
+                    break;
+
+                case '3':
+                    return;
+
+                default:
+                    System.out.println("!!!INVALID INPUT!!!");
+                    func.pause();
+                    break;
+            }
+        }
+    }
+
+    public void trainMenu(){
+        func.clear();
+        func.printHeader("Train System", '=');
+        System.out.println("1. View Trains");
+        System.out.println("2. Add Train");
+        System.out.println("3. Back");
+        func.printHeader("", '-');
+    }
+
+    public void viewTrain(){
+        func.clear();
+        viewTrains();
+        func.pause();
+    }
+
+    public void addTrain(){
+        func.clear();
+
+        String name = func.getStrLnInput("Enter train name: ");
+
+        if(isDuplicate(name)){
+            System.out.println("A train with this name already exists.\n");
+            func.pause();
+            return;
+        }
+
+        int capacity = func.getIntInput("Enter train capcity: ");
+
+        while(true){
+            System.out.println("1. Confirm");
+            System.out.println("2. Cancel");
+            char confirm = func.getChoice();
+
+            if(confirm == '1'){
+                break;
+            }else if(confirm == '2'){
+                System.out.println("Add train cancelled.\n");
+                func.pause();
+                return;
+            }else{
+                System.out.println("!!!INVALID INPUT!!!\n");
+            }
+        }
+
+        Train newTrain = new Train(nextId(), name, capacity);
+        addTrain(newTrain);
+
+        saveData();
+        System.out.println("Train added successfully\n");
+        func.pause();
+    }
 }
